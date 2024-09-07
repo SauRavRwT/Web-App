0197e4c1f9f73a27600c88f4fe4cae174b52cd8e
SauRavRwT <souravrawat142@gmail.com>
2024-09-07 18:17:32 +0530

    Added a internet permission check method...

--- app/src/main/java/com/app/webapp/MainActivity.kt
+++ app/src/main/java/com/app/webapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.app.webapp
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import com.app.webapp.utils.NetworkUtils
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mywebView: WebView
@@ -16,9 +18,17 @@ class MainActivity : AppCompatActivity() {
 
         mywebView = findViewById(R.id.webview)
         mywebView.webViewClient = MyWebClient()
+
+        // Check internet connection
+        if (NetworkUtils.isInternetAvailable(this)) {
+            // If internet is available, load the URL
             mywebView.loadUrl("Enter_your_Link_here")
+        } else {
+            // If no internet, show the alert dialog
+            showInternetAlertDialog()
+        }
 
-
+        // Enable JavaScript in WebView
         val webSettings: WebSettings = mywebView.settings
         webSettings.javaScriptEnabled = true
 
@@ -32,6 +42,19 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    // Show alert dialog to ask the user to enable the internet
+    private fun showInternetAlertDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("No Internet Connection")
+        builder.setMessage("Please enable internet access to continue using the app.")
+        builder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss() // Close the dialog
+        }
+        builder.setCancelable(false) // Make the dialog non-cancellable
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 
     inner class MyWebClient : WebViewClient() {
---
+++ app/src/main/java/com/app/webapp/utils/NetworkUtils.kt
@@ -0,0 +1,18 @@
+package com.app.webapp.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+object NetworkUtils {
+    fun isInternetAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
+    }
+}
